50aa4c718f954c88f15fe8965183d05ad04f82b6
Florian LEDOUX <ledoux.florian30@gmail.com>
2023-11-09 13:49:56 +0100

    fix

---
+++ DomusReborn/sensor_data/static/js/main.js
@@ -0,0 +1,8 @@
+function toggleDetails(element) {
+    var details = element.querySelector('.sensor-details');
+    if (details.style.display === 'none') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
