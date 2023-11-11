35e91625a0513b39b2c72a8733e4150ddaf5063b
Ledoux38 <ledoux.florian30@gmail.com>
2023-11-11 21:04:56 +0100

    add sensor

--- DomusReborn/sensor_data/views.py
+++ DomusReborn/sensor_data/views.py
@@ -11,26 +11,20 @@ def receive_sensor_data(request):
 
 
 def add_sensor(request):
-    if request.method == 'POST':
-        form = SensorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/sensors/')
-        else:
-            form = SensorForm()
-
-            return render(request, 'add_sensor.html', {'form': form})
+    form = SensorForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/sensors/')
+    return render(request, 'sensor_data/manage_sensor.html', {'form': form, 'action': 'Add'})
 
 
 def edit_sensor(request, sensor_id):
     sensor = get_object_or_404(Sensor, pk=sensor_id)
-    if request.method == 'POST':
-        form = SensorForm(request.POST, instance=sensor)
-        if form.is_valid():
-            return HttpResponseRedirect('/sensors/')
-        else:
-            form = SensorForm(instance=sensor)
-            return render(request, 'edit_sensor.html', {'form': form})
+    form = SensorForm(request.POST or None, instance=sensor)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/sensors/')
+    return render(request, 'sensor_data/manage_sensor.html', {'form': form, 'action': 'Edit', 'sensor': sensor})
 
 
 def delete_sensor(request, sensor_id):
