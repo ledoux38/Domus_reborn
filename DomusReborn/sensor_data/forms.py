64250ce646340383e2c3a23d2a0d2e83ef0310c7
Ledoux38 <ledoux.florian30@gmail.com>
2023-11-15 07:08:01 +0100

    add sensor

--- DomusReborn/sensor_data/forms.py
+++ DomusReborn/sensor_data/forms.py
@@ -7,3 +7,6 @@ class SensorForm(forms.ModelForm):
     class Meta:
         model = Sensor
         fields = ['name', 'sensor_type', 'location', 'last_reading', 'last_reading_time', 'is_active']
+        widgets = {
+            'sensor_type': forms.Select(attrs={'class': 'select-neon'})
+        }
